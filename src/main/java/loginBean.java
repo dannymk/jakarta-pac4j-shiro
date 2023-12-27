5f38013e355c9fa0437ad9aaf8d16defee38f6fd
Dan Maldonado <dmaldonado@epc-instore.com>
2023-12-27 16:10:06 -0500

    First working version of Shiro with all dependencies and configuration.

---
+++ src/main/java/loginBean.java
@@ -0,0 +1,36 @@
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+import java.io.Serializable;
+
+@SessionScoped
+@Named("loginBean")
+public class loginBean implements Serializable {
+    private String username;
+    private String password;
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String login() {
+        if (username.equals("admin") && password.equals("admin")) {
+          AuthenticationToken token = new UsernamePasswordToken(username, password);
+          SecurityUtils.getSubject().login(token);
+          return "success";
+        } else {
+          SecurityUtils.getSubject().logout();
+            return "failure";
+        }
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<br />
+<a href="secure">Go to the secure location</a>
+</body>
+</html>
